b47c3c9ff2c564e443e6756c980db2a4a8494702
Amram1997 <amramananyan9@gmail.com>
2022-04-12 11:01:29 +0400

    fix:clear commit in file

--- arguments.js
+++ arguments.js
@@ -1,6 +1,3 @@
-//Return the sum of all arguments passed to function.
-//Example: f(1,2,3,4) => 24, f(4,7) => 28, f(4) => 4
-
 function sumArguments(...val) {
   return val.reduce((prev, curr) => {
     return prev * curr;
